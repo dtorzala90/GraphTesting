049f971bc4d34841ebe75ceec38fe46301d6c582
Daniel Torzala <danieltorzala@gmail.com>
2019-04-23 21:32:08 -0700

    Initial file dump

---
+++ GraphTesting/src/GraphFrame.java
@@ -0,0 +1,133 @@
+import java.awt.Color;
+import java.awt.geom.Rectangle2D;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import org.jgraph.JGraph;
+import org.jgraph.graph.DefaultEdge;
+import org.jgraph.graph.DefaultGraphCell;
+import org.jgraph.graph.DefaultGraphModel;
+import org.jgraph.graph.GraphConstants;
+import org.jgraph.graph.GraphModel;
+
+import com.mxgraph.swing.mxGraphComponent;
+import com.mxgraph.util.mxConstants;
+import com.mxgraph.view.mxGraph;
+
+public class GraphFrame extends JFrame {
+    public static void main(String[] args) {
+        mxGraph graph = new mxGraph();
+        Object parent = graph.getDefaultParent();
+        graph.getModel().beginUpdate();
+        int size = 4;
+        try {
+            Object v0 = graph.insertVertex(parent, null, null, 5, 5, size, size);
+            Object v1 = graph.insertVertex(parent, null, null, 100, 200, size, size);
+            Object v2 = graph.insertVertex(parent, null, null, 330, 20, size, size);
+            Object v3 = graph.insertVertex(parent, null, null, 40, 160, size, size);
+            Object v4 = graph.insertVertex(parent, null, null, 240, 120, size, size);
+            graph.insertEdge(parent, null, null, v1, v2);
+            graph.insertEdge(parent, null, null, v2, v0);
+            graph.insertEdge(parent, null, null, v0, v3);
+           //graph.insertEdge(parent, id, value, source, target, style)
+        } finally {
+            graph.getModel().endUpdate();
+        }
+        mxGraphComponent graphComponent = new mxGraphComponent(graph);
+
+        GraphFrame frame = new GraphFrame();
+        frame.add(graphComponent);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
+    
+    
+//    public static void main(String[] args) {
+//
+//        // Construct Model and Graph
+//        GraphModel model = new DefaultGraphModel();
+//        JGraph graph = new JGraph(model);
+//        // Control-drag should clone selection
+//        graph.setCloneable(true);
+//
+//        // Enable edit without final RETURN keystroke
+//        graph.setInvokesStopCellEditing(true);
+//
+//        // When over a cell, jump to its default port (we only have one, anyway)
+//        graph.setJumpToDefaultPort(true);
+//
+//        // Insert all three cells in one call, so we need an array to store them
+//        DefaultGraphCell[] cells = new DefaultGraphCell[3];
+//
+//        // Create Hello Vertex
+//        cells[0] = createVertex("Hello", 20, 20, 40, 20, null, false );
+//
+//        // Create World Vertex
+//        cells[1] = createVertex("World", 140, 140, 40, 20,
+//                Color.ORANGE, true);
+//
+//        // Create Edge
+//        DefaultEdge edge = new DefaultEdge("foo");
+//        // Fetch the ports from the new vertices, and connect them with the edge
+//        edge.setSource(cells[0].getChildAt(0));
+//        edge.setTarget(cells[0].getChildAt(0));
+//        cells[2] = edge;
+//
+//        // Create Edge
+//        DefaultEdge edge1 = new DefaultEdge();
+//        // Fetch the ports from the new vertices, and connect them with the edge
+////        cells[0].addPort();
+////        cells[1].addPort();
+////        edge1.setSource(cells[1]);
+////        edge1.setTarget(cells[0]);
+////        cells[3] = edge1;
+//
+//        // Set Arrow Style for edge
+//        int arrow = GraphConstants.ARROW_CLASSIC;
+//        GraphConstants.setLineEnd(edge.getAttributes(), arrow);
+//        GraphConstants.setEndFill(edge.getAttributes(), true);
+//
+//        // Insert the cells via the cache, so they get selected
+//        graph.getGraphLayoutCache().insert(cells);
+//
+//        // Show in Frame
+//        JFrame frame = new JFrame();
+//        frame.getContentPane().add(new JScrollPane(graph));
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        frame.pack();
+//        frame.setVisible(true);
+//    }
+//
+//    public static DefaultGraphCell createVertex(String name, double x,
+//            double y, double w, double h, Color bg, boolean raised) {
+//
+//        // Create vertex with the given name
+//        DefaultGraphCell cell = new DefaultGraphCell(name);
+//
+//        // Set bounds
+//        GraphConstants.setBounds(cell.getAttributes(),
+//                new Rectangle2D.Double(x, y, w, h));
+//
+//        // Set fill color
+//        if (bg != null) {
+//            GraphConstants.setGradientColor(cell.getAttributes(), bg);
+//            GraphConstants.setOpaque(cell.getAttributes(), true);
+//        }
+//
+//        // Set raised border
+//        if (raised) {
+//            GraphConstants.setBorder(cell.getAttributes(),
+//                    BorderFactory.createRaisedBevelBorder());
+//        } else // Set black border
+//        {
+//            GraphConstants.setBorderColor(cell.getAttributes(),
+//                    Color.black);
+//        }
+//        // Add a Floating Port
+//        cell.addPort();
+//
+//        return cell;
+//    }
+//}
